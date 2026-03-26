560128de8c4fef8bd5a49b37df5f803d7daf2d4f
tomodelic21 <tomo.delic365@gmail.com>
2026-03-26 18:29:38 +0100

    drugi commit

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
 
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Novo");
+        }
+    }
+}
